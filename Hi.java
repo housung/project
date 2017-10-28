d918d0894836579e18407bddaa033fad31f17b0a
koo hou <bobibobibobi@naver.com>
2017-10-29 01:09:39 +0900

    B

--- Hi.java
+++ Hi.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Heloo.world from git!");
         SYstem.out.println("Changes after First Commit!");
+        System.out.println("BB");
  }
 }
